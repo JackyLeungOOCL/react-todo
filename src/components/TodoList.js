822eb22bca8f17bc08ce89dd4deee4b9777a805c
jacky.leung <jacky.leung@oocl.com>
2018-12-05 15:01:06 +0800

    [LEUNGJA] refactor: extract method to Todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import TodoInput from './TodoInput';
 import Todos from './Todos';
-import { connect } from "react-redux";
 
-class TodoList extends Component {
+export default class TodoList extends Component {
   render() {
     return (
       <div>
         <TodoInput/>
-        <Todos todos={this.props.todos}/>
+        <Todos/>
       </div>
     )
   }
 }
-
-const mapStateToProps = state => ({
-  todos: state.todos
-});
-
-export default connect(mapStateToProps)(TodoList);
